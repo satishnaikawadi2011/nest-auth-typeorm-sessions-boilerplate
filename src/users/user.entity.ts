d59d0d15bd7303b5f7469553a2ee0961e3c02dd2
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-05 20:28:16 +0530

    Marked username property in User entity as unique.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -4,7 +4,8 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class User {
 	@PrimaryGeneratedColumn('uuid') id: string;
 
-	@Column() username: string;
+	@Column({ unique: true })
+	username: string;
 
 	@Column() email: string;
 
