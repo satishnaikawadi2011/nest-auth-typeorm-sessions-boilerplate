aff88e4a5fe74261a9ad4a7454c8eed2b5260787
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-05 20:26:53 +0530

    Changed ids of both entities from auto increamenting to uuid based ids..

--- src/todos/todo.entity.ts
+++ src/todos/todo.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Todo {
-	@PrimaryGeneratedColumn() id: number;
+	@PrimaryGeneratedColumn('uuid') id: string;
 
 	@Column() title: string;
 
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-	@PrimaryGeneratedColumn() id: number;
+	@PrimaryGeneratedColumn('uuid') id: string;
 
 	@Column() username: string;
 
