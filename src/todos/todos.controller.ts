3cc822b302296ed0bf20a39fe5f16047eb9f7262
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-06 22:41:20 +0530

    Added route to create todo.

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { AuthGuard } from './../guards/auth.guard';
+import { CreateTodoDto } from './dtos/create-todo.dto';
+import { Body, UseGuards } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+	constructor(private todoService: TodosService) {}
+
+	@Post()
+	@UseGuards(AuthGuard)
+	createTodo(@Body() body: CreateTodoDto) {
+		return this.todoService.create(body);
+	}
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTodoDto } from './dtos/create-todo.dto';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+	constructor(@InjectRepository(Todo) private repo: Repository<Todo>) {}
+
+	create(todoDto: CreateTodoDto) {
+		const todo = this.repo.create(todoDto);
+		return this.repo.save(todo);
+	}
+}
